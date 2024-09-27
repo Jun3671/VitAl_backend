f335b50dd3ada7147a25535edd13dbf9db647fae
Jun3671 <wnsgur3027@naver.com>
2024-09-27 19:29:34 +0900

    test

--- src/main/java/VitAI/injevital/controller/aa.java
+++ src/main/java/VitAI/injevital/controller/aa.java
@@ -1,12 +1,10 @@
 package VitAI.injevital.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-public class controller {
+public class aa {
     @GetMapping("test")
     public String Test() {
-            return "success!";
-        }
-
+        return "success!";
+    }
 }

850a635956c206c40291c6eb5de26858c1d92b78
Jun3671 <wnsgur3027@naver.com>
2024-10-13 20:44:53 +0900

    :white_check_mark: commit

--- src/main/java/VitAI/injevital/controller/MemberController.java
+++ src/main/java/VitAI/injevital/controller/MemberController.java
@@ -38,6 +38,7 @@ public class MemberController {
     @PostMapping("/member/login")
     public ApiResponse login(@RequestBody LoginRequest memberDTO) {
         try {
+            System.out.println(memberDTO.getMemberEmail());
             memberService.login(memberDTO);
             return ApiResponse.success("로그인 성공");
         } catch (Exception e) {

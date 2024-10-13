380dbb96bf9da36e44f0c72251b6366f96203ae9
Jun3671 <wnsgur3027@naver.com>
2024-10-13 21:01:57 +0900

    :white_check_mark: commit

---
+++ src/main/java/VitAI/injevital/config/WebConfig.java
@@ -0,0 +1,17 @@
+package VitAI.injevital.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 프론트엔드 URL을 넣어도 됩니다.
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}

22c2f9ef1dcf6187acfe89886904d848db78e304
Jun3671 <wnsgur3027@naver.com>
2024-11-07 18:04:09 +0900

    :white_check_mark: 수정

--- src/main/java/VitAI/injevital/config/SecurityConfig.java
+++ src/main/java/VitAI/injevital/config/SecurityConfig.java
@@ -2,11 +2,28 @@ package VitAI.injevital.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger 관련 경로 예외 처리
+                .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+                .and()
+                .csrf(AbstractHttpConfigurer::disable); // CSRF 비활성화 설정
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {

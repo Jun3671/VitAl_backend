530b0f273d46b5891d8bf91378b08232c875c186
준혁 <160326131+Jun3671@users.noreply.github.com>
2026-02-06 18:14:58 +0900

    [Refactor] 스케줄링 및 JPA Auditing 기능 활성화
    - @EnableScheduling 추가: 인증코드 자동 정리 배치 작업 지원
    - @EnableJpaAuditing 추가: BaseEntity의 생성/수정 시간 자동 관리
    - 효과: 엔티티 생명주기 자동 추적 및 배치 작업 스케줄링 인프라 구축

--- src/main/java/VitAI/VitAlApplication.java
+++ src/main/java/VitAI/VitAlApplication.java
@@ -3,9 +3,13 @@ package VitAI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 
+@EnableScheduling
+@EnableJpaAuditing
 @SpringBootApplication
 public class VitAlApplication {
 
